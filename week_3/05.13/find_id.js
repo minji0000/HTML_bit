8e7e42b2cd824d59bbc30348661d23d01c0f31cf
minji0000 <bluurmi@gmail.com>
2022-05-16 18:23:55 +0900

    2022. 05. 16 DB 처음 배운 날

---
+++ week_3/05.13/find_id.js
@@ -0,0 +1,20 @@
+members_id =
+{
+    민지: { id: "pink" },
+    메리: { id: "merry" },
+    낙타: { id: "carmel" },
+    베리: { id: "yeomin" },
+    태비: { id: "juyeon" },
+    갱이: { id: "orange" },
+    뚭니: { id: "subin" },
+    거봉이: { id: "cat" },
+    두부: { id: "ssakki" },
+    민콩수니: { id: "jahyoun" },
+    예쮸니: { id: "black" },
+    예청이: { id: "sung" }
+}
+
+function find_id_codepack() {
+    let members_id = document.getElementById("id").value;
+
+}
